6284b4a1be69c1397750bab3687de85b38d4f243
Naomi80 <naomi.morville-nm-80@outlook.com>
2021-10-27 11:50:56 +0200

    1

--- src/Tableau1.js
+++ src/Tableau1.js
@@ -26,12 +26,17 @@ class Tableau1 extends Phaser.Scene{
         }
 
         //filtre film TODO élève : faire une boucle à la place des 3 lignes qui suivent
-        this.load.image('filterFilm1', 'assets/level/filters/film/frame-1.png');
-        this.load.image('filterFilm2', 'assets/level/filters/film/frame-2.png');
-        this.load.image('filterFilm3', 'assets/level/filters/film/frame-3.png');
+        for(let f=1;f<=3;f++){
+            this.load.image('filterFilm'+f, 'assets/level/filters/film/frame-'+f+'.png');
+        }
+
+        //this.load.image('filterFilm1', 'assets/level/filters/film/frame-1.png');
+        //this.load.image('filterFilm2', 'assets/level/filters/film/frame-2.png');
+        //this.load.image('filterFilm3', 'assets/level/filters/film/frame-3.png');
 
         //texture au fond  TODO élève : faire une boucle pour charger les 3 images et démontrer par la même que vous savez aller au plus simple
-        this.load.image('bg-animation-a', 'assets/level/background-2/bg-animation/bg-animation-a.png');
+        for(let bg=1;bg<=3;bg++)
+        this.load.image('bg-animation-'+bg, 'assets/level/background-2/bg-animation/bg-animation-'+bg+'.png');
 
     }
 
